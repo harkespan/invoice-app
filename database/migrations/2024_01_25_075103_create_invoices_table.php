4b9ed749192dc14c27d7a17c463a4cff06a38d55
harkespan <octone.harry@gmail.com>
2024-01-26 16:46:20 +0700

    add new column costumer_name

--- database/migrations/2024_01_25_075103_create_invoices_table.php
+++ database/migrations/2024_01_25_075103_create_invoices_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('number')->unique();
             $table->integer('user_id')->unsigned();
+            $table->string('costumer_name');
             $table->datetime('inv_date');
             $table->integer('sub_total');
             $table->integer('discount')->default(0);
